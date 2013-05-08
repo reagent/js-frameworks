3a51a3f516156f76576967e815b7e2f5531ce8ea
Patrick Reagan <reaganpr@gmail.com>
2013-05-08 13:28:20 -0600

    Use persistent SQLite connection by default

--- app/config/boot.rb
+++ app/config/boot.rb
@@ -6,7 +6,11 @@ require 'bcrypt'
 # DataMapper::Logger.new($stdout, :debug)
 
 # An in-memory Sqlite3 connection:
-DataMapper.setup(:default, 'sqlite::memory:')
+# DataMapper.setup(:default, 'sqlite::memory:')
+
+# Persistent Sqlite3 connection
+root = File.expand_path(File.dirname(__FILE__) + '/..')
+DataMapper.setup(:default, "sqlite:///#{root}/db/js-frameworks.sqlite3")
 
 require 'models'
 
